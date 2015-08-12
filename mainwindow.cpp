9d4c52dc77dc7b33074e27f9233c917664842abd
wonson <wonson@gmail.com>
2015-08-12 08:51:19 +0800

    Add menu to topButtonAddRepoExtra

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,57 @@ MainWindow::MainWindow(QWidget *parent) :
     m_ui(new Ui::MainWindow)
 {
     m_ui->setupUi(this);
+
+    initTopBar();
 }
 
 MainWindow::~MainWindow()
 {
     delete m_ui;
+}
+
+void MainWindow::initTopBar()
+{
+    // Add menu for topButtonAddRepoExtra
+    QMenu *menu = new QMenu(m_ui->topButtonAddRepoExtra);
+
+    QAction *action = new QAction("Open from Repository Manager...", menu);
+    QObject::connect(action, SIGNAL(triggered(bool)), this, SLOT(slot_addRepoFromManager()));
+    menu->addAction(action);
+
+    menu->addSeparator();
+
+    action = new QAction("Clone...", menu);
+    QObject::connect(action, SIGNAL(triggered(bool)), this, SLOT(slot_cloneRepo()));
+    menu->addAction(action);
+
+    action = new QAction("New...", menu);
+    QObject::connect(action, SIGNAL(triggered(bool)), this, SLOT(slot_addNewRepo()));
+    menu->addAction(action);
+
+    action = new QAction("Add from Existing Working Copy...", menu);
+    QObject::connect(action, SIGNAL(triggered(bool)), this, SLOT(slot_addRepoFromWorkingCopy()));
+    menu->addAction(action);
+
+    m_ui->topButtonAddRepoExtra->setMenu(menu);
+}
+
+void MainWindow::slot_addRepoFromManager()
+{
+    qDebug("slot_addRepoFromManager");
+}
+
+void MainWindow::slot_cloneRepo()
+{
+    qDebug("slot_cloneRepo");
+}
+
+void MainWindow::slot_addNewRepo()
+{
+    qDebug("slot_addNewRepo");
+}
+
+void MainWindow::slot_addRepoFromWorkingCopy()
+{
+    qDebug("slot_addRepoFromWorkingCopy");
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,8 +15,16 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+public slots:
+    void slot_addRepoFromManager();
+    void slot_cloneRepo();
+    void slot_addNewRepo();
+    void slot_addRepoFromWorkingCopy();
+
 private:
     Ui::MainWindow *m_ui;
+
+    void initTopBar();
 };
 
 #endif // MAINWINDOW_H
